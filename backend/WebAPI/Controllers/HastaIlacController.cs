111f7705671823f0794af5374d8f798ced570541
Burhan Başaran <59660549+burhan154@users.noreply.github.com>
2022-12-25 21:43:03 +0300

    Add files via upload

---
+++ backend/WebAPI/Controllers/HastaIlacController.cs
@@ -0,0 +1,90 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HastaIlacController : ControllerBase
+    {
+        private IHastaIlacService _hastaIlacService;
+        private ICurentUserService _curentUserService;
+
+        public HastaIlacController(IHastaIlacService hastaIlacService, ICurentUserService curentUserService)
+        {
+            _hastaIlacService = hastaIlacService;
+            _curentUserService = curentUserService;
+        }
+
+        [HttpGet("getallbyhastaid")]
+        public IActionResult GetAllByHastaId(int hastaId)
+        {
+            var result = _hastaIlacService.GetAllByHastaId(hastaId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAllLoggedUser()
+        {
+            var id =_curentUserService.UserId();
+            if (id == null)
+                return BadRequest("Not Authorized");
+
+            var result = _hastaIlacService.GetAllByHastaId(Convert.ToInt32(id));
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(HastaIlac hastaIlac)
+        {
+            var result = _hastaIlacService.Add(hastaIlac);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(HastaIlac hastaIlac)
+        {
+            var result = _hastaIlacService.Update(hastaIlac);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(HastaIlac hastaIlac)
+        {
+            var result = _hastaIlacService.Delete(hastaIlac);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+    
+}
---
+++ backend/WebAPI/Controllers/IlacSaatiController.cs
@@ -0,0 +1,90 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IlacSaatiController : ControllerBase
+    {
+        private IIlacSaatiService _ilacSaatiService;
+
+        public IlacSaatiController(IIlacSaatiService ilacSaatiService)
+        {
+            _ilacSaatiService = ilacSaatiService;
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int ilacSaatiId)
+        {
+            var result = _ilacSaatiService.GetById(ilacSaatiId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyilacid")]
+        public IActionResult GetListbyIlacId(int ilacId)
+        {
+            var result = _ilacSaatiService.GetListbyIlacId(ilacId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult AddIlacSaati(IlacSaati ilacSaati)
+        {
+            var result = _ilacSaatiService.Add(ilacSaati);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult DeleteIlacSaati(IlacSaati ilacSaati)
+        {
+            var result = _ilacSaatiService.Delete(ilacSaati);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult UpdateIlacSaati(IlacSaati ilacSaati)
+        {
+            var result = _ilacSaatiService.Update(ilacSaati);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("deletebyilacid")]
+        public IActionResult DeleteAllByIlacId(int ilacId)
+        {
+            var result = _ilacSaatiService.DeleteAllByIlacId(ilacId);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
