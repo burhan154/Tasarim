3f6041965935221ea079516344af08f8993b9ad6
Burhan Başaran <59660549+burhan154@users.noreply.github.com>
2022-12-25 21:40:53 +0300

    Add files via upload

---
+++ backend/Core/Entities/Concrete/UserPassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Core.Entities.Concrete
+{
+    public class UserPassword : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+    }
+}
---
+++ backend/Core/Utilities/Security/Jwt/AccessToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class AccessToken
+    {
+        //public int UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+
+    }
+}
